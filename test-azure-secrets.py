dd76928aac972c5cc95e6a518e3ab99b487800c4
BenK0lin <84003210+BenK0lin@users.noreply.github.com>
2025-02-19 11:10:04 +0200

    Update test-azure-secrets.py

--- test-azure-secrets.py
+++ test-azure-secrets.py
@@ -1,81 +1,107 @@
-# test_secrets.py
-# WARNING: This is a test file containing fake Azure credentials for secret scanning tool testing
+# Azure Active Directory Application Secret
+APP_SECRET~abcd1234-ef56-gh78-ij90-klmnopqrstuv
 
-import os
-from datetime import datetime
+# Azure Active Directory User Credential
+uid=alice@contoso.com;pwd=ExamplePass123!
 
-# Azure Access Token (JWT)
-ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6IjEyMzQ1Njc4OTAifQ.eyJhdWQiOiJodHRwczovL21hbmFnZW1lbnQuYXp1cmUuY29tIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZmFrZS10ZW5hbnQtaWQvIiwiaWF0IjoxNjE2MTUxNjE2LCJuYmYiOjE2MTYxNTE2MTYsImV4cCI6MTYxNjE1NTUxNn0.THIS_IS_A_FAKE_SIGNATURE_FOR_TESTING_123456789"
+# Azure APIM Direct Management Key
+directmanagement-8a4c2b1e9d3f7g5h
 
-# Storage Connection String
-AZURE_STORAGE_CONNECTION = "DefaultEndpointsProtocol=https;AccountName=fakestorageaccount;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;EndpointSuffix=core.windows.net"
+# Azure APIM Gateway Key
+gateway-p9f8e7d6c5b4a3g2h1
 
-# SAS Token
-sas_token = "?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2024-12-31T21:15:28Z&st=2024-01-01T13:15:28Z&spr=https&sig=FAKE9mhLUv05q2anJCyLeCYy1YhgihSNUboaHqWaGbg%3D"
+# Azure APIM Repository Key
+repository-h2g1f3e4d5c6b7a8p9
 
-class AzureConfig:
-    def __init__(self):
-        # Service Principal Secret
-        self.client_secret = "Fake8Q~test123secretkeyforazureserviceprincipalABCDEF789"
-        
-        # Registry Access Token
-        self.registry_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlRFU1QxMjM0In0.eyJqdGkiOiJmYWtlLWp0aS1pZCIsImlhdCI6MTYxNjE1MTYxNiwibmJmIjoxNjE2MTUxNjE2LCJleHAiOjE2MTYxNTU1MTYsImlzcyI6ImZha2UtcmVnaXN0cnkuYXp1cmVjci5pbyIsImF1ZCI6ImZha2UtcmVnaXN0cnkuYXp1cmVjci5pbyJ9.THIS_IS_A_FAKE_SIGNATURE_FOR_TESTING_987654321"
+# Azure APIM Subscription Key
+23d1b94f86ac5e7h9j2k4m
 
-# Function Key in dictionary
-azure_functions = {
-    "function1": {
-        "key": "lFakex88Y8xrxcExampleFunctionKeyForTestingPurposesOnly123456789",
-        "url": "https://fake-function.azurewebsites.net"
-    }
-}
-client_secret = 'bP88Q~rcBcYjzzOhg1Hnn76Wm3jGgakZiZ.8vMgR'
-# Management Certificate
-MANAGEMENT_CERT = """MIICiTCCAfKgAwIBAgIQEXAMPLEFAKECERTIFICATEMSFTw="""
+# Azure App Configuration Connection String
+Endpoint=https://example-appconfig.azconfig.io;Id=l8k7-j6h5-g4f3-d2s1;Secret=2z9y8x7w6v5u4t3s
 
-# Container Registry Password
-CONTAINER_REGISTRY_PASSWORD = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDcxNTE2MTYsImlzcyI6ImZha2UtY29udGFpbmVyLXJlZ2lzdHJ5IiwiYXVkIjoiZmFrZS1jb250YWluZXItcmVnaXN0cnkifQ.FAKE_SIGNATURE_FOR_TESTING_PURPOSES_ONLY_123"
-`client_secret=bP88Q~rcBcYjzzOhg1Hnn76Wm3jGgakZiZ.8vMgR`, // gitleaks:allow
-`client_secret=bP88Q~rcBcYjzzOhg1Hnn76Wm3jGgakZiZ.8vMgR
-`, // gitleaks:allow
-`client_secret: .IQ8Q~79R7TOWOspFnWcEG-dYt4KXqFqxK16cxr`,                                                                                              // gitleaks:allow
-		`AUTH_CLIENTSECRET = _V28Q~IC8qxmlWNpHuDm34JlbKv9LXV5MvUR3a-P`,                                                                                        // gitleaks:allow
-		`<value xsi:type="xsd:string">~Gg8Q~nVhlLi2vpg_nXBGqFsbGK-t~Hus1JmTa0y</value>`,                                                                       // gitleaks:allow
-		`"CLIENT_SECRET": "YYz7Q~Sudoqwap1PnzEBA3zqBK~i5uesDIv.C"`,                                                                                            // gitleaks:allow
-		`Set-PSUAuthenticationMethod -Type 'OpenIDConnect' -CallbackPath '/auth/oidc' -ClientId 'fake' -ClientSecret '2Vq7Q~q5VgKljZ7cb3.0sp0Apz.vOjRIPyeTr'`, // gitleaks:allow
-		`client-secret: "t028Q~-aLbmQuinnZtzbgtlEAYstnBWEmGPAoBm"`,                                                                                            // gitleaks:allow
-		`"cas.authn.azure-active-directory.client-secret=qHF8Q~PCM5HhMoyTFc5TYEomnzR6Kim9UJhe8a.P",`,                                                          // gitleaks:allow
-		`"line": "client_srt = \"qpF8Q~PCM5MhMoyTFc5TYEomnYRUKim9UJhe8a2P\";",`,                                                                               // gitleaks:allow
-		`"client_secret":       acctest.Representation{RepType: acctest.Required, Create: 'dO29Q~F5-VwnW.lZdd11xFF_t5NAXCaGwDl9NbT1'},`,                       // gitleaks:allow
-		`Example= GN.7Q~4AkLZBNEbz4Jxlm~O5G6SsyFxYg6zMR`,                                                                                                      // gitleaks:allow
-		`"the_value": "QtT8Q~9C-_Ij~RouHVpD2Tuf3oHWGh.DQ3kcjbAn"`,                                                                                             // gitleaks:allow
-		`QtT8Q~9C-_Ij~RouHVpD2Tuf3oHWGh.DQ3kcjbAn`,                                                                                                            // gitleaks:allow
-		`(use the client secret: QtT8Q~9C-_Ij~RouHVpD2Tuf3oHWGh.DQ3kcjbAn)`,                                                                                   // gitleaks:allow
-		`(QtT8Q~9C-_Ij~RouHVpD2Tuf3oHWGh.DQ3kcjbAn)`,                                                                                                          // gitleaks:allow
-def get_storage_credentials():
-    # Secrets in function return
-    return {
-        "connection_string": "DefaultEndpointsProtocol=https;AccountName=fakestorageaccount;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;EndpointSuffix=core.windows.net",
-        "sas_token": "?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2024-12-31T21:15:28Z&st=2024-01-01T13:15:28Z&spr=https&sig=FAKE9mhLUv05q2anJCyLeCYy1YhgihSNUboaHqWaGbg%3D"
-    }
+# Azure Batch Key
+sv=2020-01-01&ss=b&srt=s&sp=rwdl&se=2025-01-01T00:00:00Z&st=2024-01-01T00:00:00Z&spr=https&sig=batch%2Bkey%example123
 
-def initialize_services():
-    # Multiple secrets in function variables
-    access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6IjEyMzQ1Njc4OTAifQ.eyJhdWQiOiJodHRwczovL21hbmFnZW1lbnQuYXp1cmUuY29tIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZmFrZS10ZW5hbnQtaWQvIiwiaWF0IjoxNjE2MTUxNjE2LCJuYmYiOjE2MTYxNTE2MTYsImV4cCI6MTYxNjE1NTUxNn0.THIS_IS_A_FAKE_SIGNATURE_FOR_TESTING_123456789"
-    registry_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlRFU1QxMjM0In0.eyJqdGkiOiJmYWtlLWp0aS1pZCIsImlhdCI6MTYxNjE1MTYxNiwibmJmIjoxNjE2MTUxNjE2LCJleHAiOjE2MTYxNTU1MTYsImlzcyI6ImZha2UtcmVnaXN0cnkuYXp1cmVjci5pbyIsImF1ZCI6ImZha2UtcmVnaXN0cnkuYXp1cmVjci5pbyJ9.THIS_IS_A_FAKE_SIGNATURE_FOR_TESTING_987654321"
-    client_secret = "Fake8Q~test123secretkeyforazureserviceprincipalABCDEF789"
-    
-    print(f"Initialized with token: {access_token}")  # Logging secrets - should be detected
+# Azure Cache for Redis Access Key
+redisaccess=zlk4j3h2g1f9d8s7a6
 
-# Comments containing secrets (some scanners should detect these)
-# Access Token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6IjEyMzQ1Njc4OTAifQ.eyJhdWQiOiJodHRwczovL21hbmFnZW1lbnQuYXp1cmUuY29tIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZmFrZS10ZW5hbnQtaWQvIiwiaWF0IjoxNjE2MTUxNjE2LCJuYmYiOjE2MTYxNTE2MTYsImV4cCI6MTYxNjE1NTUxNn0.THIS_IS_A_FAKE_SIGNATURE_FOR_TESTING_123456789
-# Cert: MIICiTCCAfKgAwIBAgIQEXAMPLEFAKECERTIFICATEMSFTw=
+# Azure Communication Services Connection String
+endpoint=https://example.communication.azure.com/;accesskey=1a2b3c4d5e6f7g8h9i0j
 
-def main():
-    config = AzureConfig()
-    creds = get_storage_credentials()
-    initialize_services()
-    
-    print("Test application initialized with fake credentials")
+# Azure Container Registry Key
+registrykey=cr1234567890abcdef
 
-if __name__ == "__main__":
-    main()
+# Azure CosmosDB Key
+cosmosdb=7h8j9k0l1m2n3p4q5r
+
+# Azure DevOps Personal Access Token
+pat_v2.0.12345abcdefghijklmnopqrstuvwxyz0123
+
+# Azure Event Hub Key
+Endpoint=sb://example.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=evhub123456789
+
+# Azure Function Key
+functionkey=0z9y8x7w6v5u4t3s2r1q
+
+# Azure IoT Device Connection String
+HostName=example.azure-devices.net;DeviceId=myDevice;SharedAccessKey=iotdevice123456
+
+# Azure IoT Device Key
+iotdevicekey=9q8w7e6r5t4y3u2i1o
+
+# Azure IoT Device Provisioning Key
+provisionkey=2w3e4r5t6y7u8i9o0p
+
+# Azure IoT Hub Connection String
+HostName=example.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=iothub123456789
+
+# Azure IoT Hub Key
+iothubkey=5t6y7u8i9o0p1a2s3d
+
+# Azure IoT Provisioning Connection String
+HostName=example.azure-devices-provisioning.net;SharedAccessKeyName=provisioningowner;SharedAccessKey=provision123456
+
+# Azure Management Certificate
+managementcert=mc1234567890abcdef
+
+# Azure ML Web Service Classic Key
+mlwebservice=mls7h8j9k0l1m2n3p4
+
+# Azure OpenAI Key
+sk-aoai4k3j2h1g9f8d7s6a5
+
+# Azure Relay Key
+relaykey=rk2s3d4f5g6h7j8k9l
+
+# Azure SAS Token
+sv=2020-01-01&ss=b&srt=s&sp=rwdl&se=2025-01-01T00:00:00Z&st=2024-01-01T00:00:00Z&spr=https&sig=sas%2Btoken%example123
+
+# Azure Search Admin Key
+searchadmin=sa3f4g5h6j7k8l9p0
+
+# Azure Search Query Key
+searchquery=sq7g8h9j0k1l2p3m4
+
+# Azure Service Bus Key
+Endpoint=sb://example.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=servicebus123456
+
+# Azure SignalR Connection String
+Endpoint=https://example.service.signalr.net;AccessKey=signalr1234567890;Version=1.0;
+
+# Azure SQL Connection String
+Server=example.database.windows.net;Database=mydb;User ID=admin;Password=sqlpass123!;
+
+# Azure SQL Password
+SqlP@ssw0rd123!
+
+# Azure Storage Account Key
+DefaultEndpointsProtocol=https;AccountName=example;AccountKey=storageacc123456==;EndpointSuffix=core.windows.net
+
+# Azure Web PubSub Connection String
+Endpoint=https://example.webpubsub.azure.com;AccessKey=pubsub123456789;Version=1.0;
+
+# Microsoft Azure Entra ID Token
+eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSJ9.example
+
+# Microsoft Corporate Network User Credential
+domain\username:CorpP@ssw0rd123!
